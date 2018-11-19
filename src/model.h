0ec9d6a170abee0d07feb39bd56693770b5e81e5
Gleb Karpushkin <rugleb@gmail.com>
2018-11-19 23:37:39 +0300

    created structure for dataset

--- src/model.h
+++ src/model.h
@@ -1,10 +1,23 @@
 #ifndef NN_CPP_MODEL_H
 #define NN_CPP_MODEL_H
 
+#include "support.h"
+
+
+struct Set {
+    D_VECTOR X;
+    D_VECTOR Y;
+};
+
 
 class Model {
+    std::vector<Set> dataset;
+    std::vector<D_VECTOR> W;
 public:
     Model();
+    void train(std::vector<Set>, double);
+    double activate(double);
+    D_VECTOR activate(D_VECTOR);
 };
 
 
