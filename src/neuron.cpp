39a19bb5d91bc0207be91d69d54455b0d2ad4c09
Gleb Karpushkin <rugleb@gmail.com>
2018-11-20 20:26:42 +0300

    updated Neuron class

--- src/neuron.cpp
+++ src/neuron.cpp
@@ -3,3 +3,17 @@
 Neuron::Neuron() {
 
 }
+
+double Neuron::activate(double x) {
+    return x;
+}
+
+double Neuron::calc(D_VECTOR input, D_VECTOR weights) {
+    double y = 0.;
+
+    for (size_t i = 0; i < input.size(); i++) {
+        y += input[i] * weights[i];
+    }
+
+    return this->activate(y);
+}
--- src/neuron.h
+++ src/neuron.h
@@ -2,9 +2,14 @@
 #define NEURAL_NETWORK_NEURON_H
 
 
+#include "support.h"
+
 class Neuron {
 public:
     Neuron();
+
+    double activate(double);
+    double calc(D_VECTOR, D_VECTOR);
 };
 
 
