705c7b5f89d7767998fc633aa523665f158b2b02
Gleb Karpushkin <rugleb@gmail.com>
2018-11-21 00:36:20 +0300

    deleted const specifications

--- src/Layer.cpp
+++ src/Layer.cpp
@@ -5,12 +5,12 @@ Layer::Layer()
 
 }
 
-void Layer::addNeuron(const Neuron neuron)
+void Layer::addNeuron(Neuron neuron)
 {
     this->neurons.push_back(neuron);
 }
 
-void Layer::addNeurons(const Neuron neuron, const size_t count)
+void Layer::addNeurons(Neuron neuron, size_t count)
 {
     for (size_t i = 0; i < count; i++) {
         this->addNeuron(neuron);
