a624751b3db5dbfb1e2a0577ff43eba9eb15e63f
Gleb Karpushkin <rugleb@gmail.com>
2018-11-29 23:23:26 +0300

    created Layer constructor

--- src/Layer.cpp
+++ src/Layer.cpp
@@ -1,1 +1,6 @@
 #include "Layer.h"
+
+Layer::Layer(unsigned int units, callable activation) {
+    this->units = units;
+    this->activation = activation;
+}
--- src/Layer.h
+++ src/Layer.h
@@ -2,8 +2,14 @@
 #define NEURAL_NETWORK_LAYER_H
 
 
-class Layer {
+typedef double (*callable) (double);
 
+class Layer {
+protected:
+    unsigned int units;
+    callable activation;
+public:
+    Layer(unsigned int units, callable activation);
 };
 
 
