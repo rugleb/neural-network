83512ec98ac48c322c980347f68faeea7d95f803
Gleb Karpushkin <rugleb@gmail.com>
2018-11-29 23:16:12 +0300

    deleted all

--- main.cpp
+++ main.cpp
@@ -1,35 +1,4 @@
-#include <iostream>
-
-#include "src/Model.h"
-#include "src/Support.h"
-
-#define DATASET std::vector<Set>
-
-
-DATASET generate(unsigned int count)
-{
-    DATASET dataset(count);
-
-    for (std::size_t i = 0; i < count; i++) {
-        D_VECTOR x = {0., 1., 2.};
-        D_VECTOR y = {0., 1., 2.};
-
-        Set set = {x, y};
-        dataset[i] = set;
-    }
-
-    return dataset;
-}
-
 int main()
 {
-    Layer layer(Neuron(), 2);
-
-    Model model;
-    model.addLayer(layer);
-
-    DATASET dataset = generate(1000);
-    model.train(dataset, 1e-3);
-
     return 0;
 }
