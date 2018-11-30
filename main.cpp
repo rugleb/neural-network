35c602007bed857da487650c1dd74bb8d80637ca
Gleb Karpushkin <rugleb@gmail.com>
2018-11-30 15:45:03 +0300

    added generation training dataset

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include "src/Model.h"
 
+dataset generate(unsigned int size)
+{
+    dataset data(size);
+
+    for (unsigned int i = 0; i < size; i++) {
+        set set;
+
+        set.X = { .0, .1, .2 };
+        set.Y = { (double) i };
+
+        data[i] = set;
+    }
+
+    return data;
+}
+
 int main()
 {
     Model model;
@@ -7,7 +23,7 @@ int main()
     model.add(Layer(10, linear));
     model.add(Layer(5, relu));
 
-    dataset train;
+    dataset train = generate((unsigned int) 1e+6);
     model.fit(train, 20, 1e-5);
 
     return 0;
