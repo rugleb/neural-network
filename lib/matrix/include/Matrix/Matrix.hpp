fcd18fc9a8d01ff787799d05593667507197c9b6
Gleb Karpushkin <rugleb@gmail.com>
2019-12-02 14:26:19 +0300

    created operators for scalars

--- lib/matrix/include/Matrix/Matrix.hpp
+++ lib/matrix/include/Matrix/Matrix.hpp
@@ -3,9 +3,14 @@
 
 #include <tuple>
 #include <vector>
+#include <functional>
 
 
 typedef std::tuple<std::size_t, std::size_t> Dim;
+
+
+template <typename T>
+using Data = std::vector<std::vector<T>>;
 
 
 template <typename T>
@@ -13,34 +18,43 @@ class Matrix
 {
 public:
     explicit Matrix(std::size_t rows, std::size_t cols, T initial = 0);
-    explicit Matrix(const std::vector<std::vector<T>> & data);
+    explicit Matrix(const Data<T> & data);
 
-    std::vector<std::vector<T>> data() const;
+    Data<T> data() const;
 
-    std::size_t nRows();
-    std::size_t nCols();
+    [[nodiscard]] std::size_t nRows() const;
+    [[nodiscard]] std::size_t nCols() const;
 
     Dim dim();
+
+    std::vector<T> & operator[] (std::size_t i);
 
     bool operator==(const Matrix<T> & other) const;
     bool operator!=(const Matrix<T> & other) const;
 
+    Matrix<T> operator+(T scalar) const;
+    Matrix<T> operator-(T scalar) const;
+    Matrix<T> operator*(T scalar) const;
+    Matrix<T> operator/(T scalar) const;
+
+    Matrix<T> map(std::function<T(T)> f) const;
+    Matrix<T> map(T scalar, std::function<T(T, T)> f) const;
+
+
 protected:
-    std::vector<std::vector<T>> m_data;
+    Data<T> m_data;
 };
 
 
 template <typename T>
 Matrix<T>::Matrix(std::size_t rows, std::size_t cols, T initial)
 {
-    auto row = std::vector<T>(cols, initial);
-
-    m_data = std::vector<std::vector<T>>(rows, row);
+    m_data = Data<T>(rows, std::vector<T>(cols, initial));
 }
 
 
 template <typename T>
-Matrix<T>::Matrix(const std::vector<std::vector<T>> & data)
+Matrix<T>::Matrix(const Data<T> & data)
     : m_data(data)
 {
     //
@@ -48,21 +62,21 @@ Matrix<T>::Matrix(const std::vector<std::vector<T>> & data)
 
 
 template <typename T>
-std::vector<std::vector<T>> Matrix<T>::data() const
+Data<T> Matrix<T>::data() const
 {
     return m_data;
 }
 
 
 template <typename T>
-std::size_t Matrix<T>::nRows()
+std::size_t Matrix<T>::nRows() const
 {
     return m_data.size();
 }
 
 
 template <typename T>
-std::size_t Matrix<T>::nCols()
+std::size_t Matrix<T>::nCols() const
 {
     return m_data.front().size();
 }
@@ -79,6 +93,13 @@ Dim Matrix<T>::dim()
 
 
 template <typename T>
+std::vector<T> & Matrix<T>::operator[](std::size_t i)
+{
+    return m_data[i];
+}
+
+
+template <typename T>
 bool Matrix<T>::operator==(const Matrix<T> & other) const
 {
     return m_data == other.data();
@@ -89,6 +110,61 @@ template <typename T>
 bool Matrix<T>::operator!=(const Matrix<T> & other) const
 {
     return m_data != other.data();
+}
+
+
+template <typename T>
+Matrix<T> Matrix<T>::operator+(T scalar) const
+{
+    return map(scalar, std::plus<T>());
+}
+
+
+template <typename T>
+Matrix<T> Matrix<T>::operator-(T scalar) const
+{
+    return map(scalar, std::minus<T>());
+}
+
+
+template <typename T>
+Matrix<T> Matrix<T>::operator*(T scalar) const
+{
+    return map(scalar, std::multiplies<T>());
+}
+
+
+template <typename T>
+Matrix<T> Matrix<T>::operator/(T scalar) const
+{
+    return map(scalar, std::divides<T>());
+}
+
+
+template <typename T>
+Matrix<T> Matrix<T>::map(std::function<T(T)> f) const
+{
+    auto rows = nRows();
+    auto cols = nCols();
+
+    auto result = Matrix<T>(rows, cols);
+
+    for (std::size_t i = 0; i < rows; i++) {
+        for (std::size_t j = 0; j < cols; j++) {
+            result[i][j] = f(m_data[i][j]);
+        }
+    }
+
+    return result;
+}
+
+
+template <typename T>
+Matrix<T> Matrix<T>::map(T scalar, std::function<T(T, T)> f) const
+{
+    return map([&] (const T & item) {
+        return f(item, scalar);
+    });
 }
 
 
--- lib/tests/matrix/TestMatrix.cpp
+++ lib/tests/matrix/TestMatrix.cpp
@@ -42,3 +42,63 @@ TEST(MatrixTestCase, testOperatorNotEqualToMatrix)
 
     ASSERT_NE(m1, m2);
 }
+
+
+TEST(MatrixTestCase, testOperatorAdditionByScalar)
+{
+    std::size_t rows = 2;
+    std::size_t cols = 3;
+
+    data_t initial = 5;
+    data_t scalar = 2;
+
+    auto m1 = Matrix<data_t>(rows, cols, initial) + scalar;
+    auto m2 = Matrix<data_t>(rows, cols, initial + scalar);
+
+    ASSERT_EQ(m1, m2);
+}
+
+
+TEST(MatrixTestCase, testOperatorSubtractionByScalar)
+{
+    std::size_t rows = 2;
+    std::size_t cols = 3;
+
+    data_t initial = 5;
+    data_t scalar = 2;
+
+    auto m1 = Matrix<data_t>(rows, cols, initial) - scalar;
+    auto m2 = Matrix<data_t>(rows, cols, initial - scalar);
+
+    ASSERT_EQ(m1, m2);
+}
+
+
+TEST(MatrixTestCase, testOperatorMultiplicationByScalar)
+{
+    std::size_t rows = 2;
+    std::size_t cols = 3;
+
+    data_t initial = 5;
+    data_t scalar = 2;
+
+    auto m1 = Matrix<data_t>(rows, cols, initial) * scalar;
+    auto m2 = Matrix<data_t>(rows, cols, initial * scalar);
+
+    ASSERT_EQ(m1, m2);
+}
+
+
+TEST(MatrixTestCase, testOperatorDivisionByScalar)
+{
+    std::size_t rows = 2;
+    std::size_t cols = 3;
+
+    data_t initial = 4;
+    data_t scalar = 2;
+
+    auto m1 = Matrix<data_t>(rows, cols, initial) / scalar;
+    auto m2 = Matrix<data_t>(rows, cols, initial / scalar);
+
+    ASSERT_EQ(m1, m2);
+}
