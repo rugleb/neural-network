43e54c7b7d3a4d6a0c15cc908bca11eacc382d42
Gleb Karpushkin <rugleb@gmail.com>
2019-12-03 16:07:41 +0300

    formatting

--- cmake/googletest-download.cmake
+++ cmake/googletest-download.cmake
@@ -1,18 +1,16 @@
 cmake_minimum_required(VERSION 3.5)
 
-project(googletest-download NONE)
+project(googletest NONE)
 
 include(ExternalProject)
 
 ExternalProject_Add(googletest
-    SOURCE_DIR "@GOOGLETEST_DOWNLOAD_ROOT@/googletest-src"
-    BINARY_DIR "@GOOGLETEST_DOWNLOAD_ROOT@/googletest-build"
-    GIT_REPOSITORY
-    https://github.com/google/googletest.git
-    GIT_TAG
-    release-1.8.0
+    SOURCE_DIR        "@GTEST_DIR@/googletest-src"
+    BINARY_DIR        "@GTEST_DIR@/googletest-build"
+    GIT_REPOSITORY    https://github.com/google/googletest.git
+    GIT_TAG           release-1.8.0
     CONFIGURE_COMMAND ""
-    BUILD_COMMAND ""
-    INSTALL_COMMAND ""
-    TEST_COMMAND ""
+    BUILD_COMMAND     ""
+    INSTALL_COMMAND   ""
+    TEST_COMMAND      ""
 )
